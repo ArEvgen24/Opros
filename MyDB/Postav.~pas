f29cfe55f4ebf45d950342941aa094adfe9e4fd5
Arharov E <arharovevgenij54@gmail.com>
2024-11-20 23:04:38 +1000

    добавление форм

---
+++ MyDB/Postav.~pas
@@ -0,0 +1,24 @@
+unit Postav;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TTPostav = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  TPostav: TTPostav;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ MyDB/Prihod.pas
@@ -0,0 +1,24 @@
+unit Prihod;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TTPrihod = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  TPrihod: TTPrihod;
+
+implementation
+
+{$R *.dfm}
+
+end.
